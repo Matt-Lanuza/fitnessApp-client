ff7d7b303af9d4ff0589206cf70033771cddca8b
matt.lanuza09 <matt.lanuza09@gmail.com>
2024-11-29 09:19:28 +0800

    add completeworkoutstatus

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,19 @@
 import { Button, Row, Col } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 export default function Banner(){
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/workout');
+    };
+
     return(
         <Row>
             <Col className="text-center py-5">
                 <h1>Welcome to FitnessPal</h1>
                 <p>Every journey begins with a single step. Start yours today and achieve your fitness goals!</p>
-                <Button variant="danger">Start Your Fitness Journey</Button>
+                <Button variant="danger" onClick={handleClick}>Start Your Fitness Journey</Button>
             </Col>
         </Row>
     )
